4c2b58933babde048ea606b762d2477c59f5abc9
Tetiana Ustymenko <ustimenko.tetana7@gmail.com>
2025-04-20 19:05:48 +0300

    this code

--- js/task-01.js
+++ js/task-01.js
@@ -6,6 +6,3 @@ item.forEach(element => {
     console.log(`Category: ${element.firstElementChild.textContent}`);
     console.log(`Elements: ${element.lastElementChild.children.length}`);
 })
-  
-
-
