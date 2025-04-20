043166a13cd4af983d72f4d499bc79aa6bfb181d
Tetiana Ustymenko <ustimenko.tetana7@gmail.com>
2025-04-20 12:37:22 +0300

    new commit

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+// Завдання 1
+
+document.addEventListener("DOMContentLoaded", () => {
+    const categories = document.querySelectorAll("#categories .item");
+    console.log(`Number of categories: ${categories.length}`);
+  
+    categories.forEach((category) => {
+      const title = category.querySelector("h2").textContent;
+      const elementsCount = category.querySelectorAll("ul li").length;
+  
+      console.log(`Category: ${title}`);
+      console.log(`Elements: ${elementsCount}`);
+    });
+  });
+  
+
+
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+ // Завдання 3
+ const input = document.getElementById("name-input");
+ const output = document.getElementById("name-output");
+
+ input.addEventListener("input", () => {
+   const trimmedValue = input.value.trim();
+
+   output.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+ });
+ 
